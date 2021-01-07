7e3bb41844e8231b178135bf7f2b9d51ca06b62e
Emilien Durieu (edu) <edu@odoo.com>
2021-01-07 12:03:55 +0100

    [IMP] rename 'justify' to 'align'

--- commands/align.js
+++ commands/align.js
@@ -6,17 +6,17 @@ import {
 } from "../utils/utils.js";
 
 
-Text.prototype.oJustify = function (offset, mode) {
-    this.parentElement.oJustify(childNodeIndex(this), mode);
+Text.prototype.oAlign = function (offset, mode) {
+    this.parentElement.oAlign(childNodeIndex(this), mode);
 };
 /**
  * This does not check for command state 
  * @param {*} offset 
  * @param {*} mode 'left', 'right', 'center' or 'justify'
  */
-HTMLElement.prototype.oJustify = function (offset, mode) {
+HTMLElement.prototype.oAlign = function (offset, mode) {
     if (!isBlock(this)) {
-        return this.parentElement.oJustify(childNodeIndex(this), mode);
+        return this.parentElement.oAlign(childNodeIndex(this), mode);
     }
     this.style.textAlign = mode;
 };
