3ad8e1b32d1e1ef7d8641aa235bdcf79fff98752
Nicolas Bayet <nicolas.bayet@gmail.com>
2021-01-07 16:39:31 +0100

    [IMP] indentation

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 export default {
-  input: 'editor.js',
-  output: {
-    file: 'build/odoo-editor-bundle.js',
-    format: 'iife',
-    name: 'exportVariable',
-    banner: "odoo.define('web_editor.odoo-editor', (function(require) {",
-    footer: 'return exportVariable;\n}));',
-  }
+    input: 'editor.js',
+    output: {
+        file: 'build/odoo-editor-bundle.js',
+        format: 'iife',
+        name: 'exportVariable',
+        banner: "odoo.define('web_editor.odoo-editor', (function(require) {",
+        footer: 'return exportVariable;\n}));',
+    },
 };
