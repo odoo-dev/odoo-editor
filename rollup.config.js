afbef317f751231c49a1b70d3a0beb03890ed59c
Antoine Guenet <age@odoo.com>
2021-01-28 14:26:32 +0100

    [ADD] export css with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,21 @@
-export default {
-    input: 'src/editor.js',
-    output: {
-        file: 'build/odoo-editor-bundle.js',
-        format: 'iife',
-        name: 'exportVariable',
-        banner: "odoo.define('web_editor.odoo-editor', (function(require) {",
-        footer: 'return exportVariable;\n}));',
+import css from 'rollup-plugin-css-only';
+
+export default [
+    {
+        input: 'src/editor.js',
+        output: {
+            file: 'build/odoo-editor-bundle.js',
+            format: 'iife',
+            name: 'exportVariable',
+            banner: "odoo.define('web_editor.odoo-editor', (function(require) {",
+            footer: 'return exportVariable;\n}));',
+        },
     },
-};
+    {
+        input: 'src/style.css',
+        output: {
+            file: 'build/odoo-editor-bundle.css',
+        },
+        plugins: [css({ output: 'odoo-editor-bundle.css' })],
+    },
+];
