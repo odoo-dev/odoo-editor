c20d0844001f12df87679817e2f778408bc1008d
Emilien Durieu (edu) <edu@odoo.com>
2021-02-05 15:55:33 +0100

    [FIX] fix font-awesome deletion on ff, use html entities for zws

--- src/utils/sanitize.js
+++ src/utils/sanitize.js
@@ -108,7 +108,7 @@ class Sanitize {
         // Sanitize font awesome elements
         if (isFontAwesome(node)) {
             // Ensure a zero width space is present inside the FA element.
-            if (node.innerHTML !== '​') node.innerHTML = '​';
+            if (node.innerHTML !== '&#x200B;') node.innerHTML = '&#x200B;';
         }
 
         // Sanitize media elements
