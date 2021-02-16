41d8be0d44dc7a88ad2b5002d535a3cf2c6ec156
Nicolas Bayet <nicolas.bayet@gmail.com>
2021-02-16 17:29:15 +0100

    [FIX] insure selection on toolbar

--- src/editor.js
+++ src/editor.js
@@ -1163,11 +1163,13 @@ export class OdooEditor extends EventTarget {
                 paragraphDropdownButton.classList.toggle(newClass, isStateTrue);
             }
         }
-        const closestsStartContainer = closestElement(sel.getRangeAt(0).startContainer, '*');
-        const selectionStartStyle = getComputedStyle(closestsStartContainer);
-        const fontSizeValue = this.toolbar.querySelector('#fontSizeCurrentValue');
-        if (fontSizeValue) {
-            fontSizeValue.innerHTML = /\d+/.exec(selectionStartStyle.fontSize).pop();
+        if (sel.rangeCount) {
+            const closestsStartContainer = closestElement(sel.getRangeAt(0).startContainer, '*');
+            const selectionStartStyle = getComputedStyle(closestsStartContainer);
+            const fontSizeValue = this.toolbar.querySelector('#fontSizeCurrentValue');
+            if (fontSizeValue) {
+                fontSizeValue.innerHTML = /\d+/.exec(selectionStartStyle.fontSize).pop();
+            }
         }
         this._updateColorpickerLabels();
         let block = closestBlock(sel.anchorNode);
@@ -1182,7 +1184,9 @@ export class OdooEditor extends EventTarget {
             ['checklist', 'CL', true],
         ]) {
             const button = this.toolbar.querySelector('#' + style);
-            if (button) {
+            if (button && !block) {
+                button.classList.toggle('active', false);
+            } else if (button) {
                 const isActive = isList
                     ? block.tagName === 'LI' && getListMode(block.parentElement) === tag
                     : block.tagName === tag;
@@ -1213,8 +1217,9 @@ export class OdooEditor extends EventTarget {
         const hiliteColorInput = this.toolbar.querySelector('#hiliteColor input');
         if (hiliteColorInput) {
             const sel = this.document.defaultView.getSelection();
-            const startContainer = sel.getRangeAt(0).startContainer;
-            let closestBgColor = closestElement(startContainer, '[style*="background-color"]');
+            const startContainer = sel.rangeCount && sel.getRangeAt(0).startContainer;
+            let closestBgColor =
+                startContainer && closestElement(startContainer, '[style*="background-color"]');
             const hasBgColorStyle = !!closestBgColor;
             closestBgColor = closestBgColor || closestElement(startContainer);
             const hiliteColor = hasBgColorStyle
