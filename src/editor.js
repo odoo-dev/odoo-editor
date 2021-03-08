odoo-dev/odoo-editor

commit be5dd0842d5e8380db3772683503c16aaeaf4afd

[FIX] link full selection, deletion and insertion

src/editor.js
@@ -76,8 +76,11 @@ export class OdooEditor extends EventTarget {
         if (typeof this.options.toSanitize === 'undefined') {
             this.options.toSanitize = true;
         }
-        if (typeof this.options.setContentEditable === 'undefined') {
-            this.options.setContentEditable = true;
+        if (typeof this.options.isRootEditable === 'undefined') {
+            this.options.isRootEditable = true;
+        }
+        if (typeof this.options.getContentEditableAreas === 'undefined') {
+            this.options.getContentEditableAreas = () => [];
         }
 
         this.document = options.document || document;
@@ -99,9 +102,23 @@ export class OdooEditor extends EventTarget {
         this._observerTimeoutUnactive = new Set();
 
         // set contenteditable before clone as FF updates the content at this point
-        if (this.options.setContentEditable) {
-            dom.setAttribute('contenteditable', this.options.setContentEditable);
-        }
+        this._activateContenteditable();
+
+        // When selecting all the text within a link then triggering delete or
+        // inserting a character, the cursor and insertion is outside the link.
+        // To avoid this problem, we make all editable zone become uneditable
+        // except the link. Then when cliking outside the link, reset the
+        // editable zones.
+        this.dom.addEventListener('mousedown', e => {
+            this.automaticStepSkipStack();
+            const link = closestElement(e.target, 'a');
+            if (link) {
+                this._stopContenteditable();
+                link.setAttribute('contenteditable', 'true');
+            } else {
+                this._activateContenteditable();
+            }
+        });
         this.vdom = dom.cloneNode(true);
         this.vdom.removeAttribute('contenteditable');
         this.idSet(dom, this.vdom);
@@ -1284,6 +1301,26 @@ export class OdooEditor extends EventTarget {
                 return error;
             } else {
                 throw error;
+            }
+        }
+    }
+
+    _activateContenteditable() {
+        this.dom.setAttribute('contenteditable', this.options.isRootEditable);
+
+        for (const node of this.options.getContentEditableAreas()) {
+            if (!node.isContentEditable) {
+                node.setAttribute('contenteditable', true);
+            }
+        }
+    }
+    _stopContenteditable() {
+        if (this.options.isRootEditable) {
+            this.dom.setAttribute('contenteditable', !this.options.isRootEditable);
+        }
+        for (const node of this.options.getContentEditableAreas()) {
+            if (node.getAttribute('contenteditable') === 'true') {
+                node.setAttribute('contenteditable', false);
             }
         }
     }

commit 19088c55ef3bdfbc5d02d9aa5c285d5e8b7efb6f

[FIX] unlink was broken after be5dd084

src/editor.js
@@ -1055,6 +1055,13 @@ export class OdooEditor extends EventTarget {
 
     _unlink(offset, content) {
         const sel = this.document.defaultView.getSelection();
+        // we need to remove the contentEditable isolation of links
+        // before we apply the unlink, otherwise the command is not performed
+        // because the content editable root is the link
+        const closestEl = closestElement(sel.focusNode);
+        if (closestEl.tagName === 'A' && closestEl.getAttribute('contenteditable') === 'true') {
+            this._activateContenteditable();
+        }
         if (sel.isCollapsed) {
             const cr = preserveCursor(this.document);
             const node = closestElement(sel.focusNode, 'a');