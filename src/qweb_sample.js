fb6b3b74e92ae21f420b6bcb5c0e8e2efdbc7ed3
Nicolas Bayet <nicolas.bayet@gmail.com>
2021-04-28 17:18:54 +0200

    temp

--- src/qweb_sample.js
+++ src/qweb_sample.js
@@ -2,7 +2,9 @@ export const qwebSample = /* xml */ `
 <h1>Qweb examples</h1>
 <div>
     <t t-set="foo1" t-value="2 + 1"></t>
-    <t t-esc="foo1"></t>
+    <t t-esc="object">foo</t>
+    <t t-raw="object">foo_raw</t>
+    <t t-esc="invisible"></t>
 </div>
 
 
@@ -19,6 +21,19 @@ export const qwebSample = /* xml */ `
     </t>
     <t t-elif="condition">
         <p>elif2.1</p>
+    </t>
+    <t t-else="condition">
+        <p>elif2.1</p>
+
+        <t t-if="condition">
+            <p>if2.1.1</p>
+        </t>
+        <t t-elif="condition">
+            <p>elif2.1.2</p>
+        </t>
+        <t t-else="condition">
+            <p>elif2.1.3</p>
+        </t>
     </t>
 </div>
 
@@ -55,3 +70,27 @@ export const qwebSample = /* xml */ `
     <t t-raw="0"></t>
 </div>
 `;
+
+
+<t t-set="" t-value=""/>
+
+t-esc="record."
+t-raw="record."
+<t t-esc="record."></t>
+<t t-raw="record."></t>
+
+<t t-if="record.">
+</t>
+<t t-else="">
+</t>
+
+<t t-if="record.">
+</t>
+<t t-elif="record.">
+</t>
+<t t-else="">
+</t>
+
+t-foreach="" t-as=""
+<p t-foreach="" t-as="">
+</p> 
