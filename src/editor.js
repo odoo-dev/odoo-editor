9f8cb1b4bf3a3ee5a1f2d354cf79c934e5e15a9d
Sébastien Geelen (sge) <sge@odoo.com>
2021-04-30 16:03:36 +0200

    [IMP] toolbar: add public method to update the toolbar position
    Will be used in Odoo when scrolling in the main page must change the editor toolbar visual position.

--- src/editor.js
+++ src/editor.js
@@ -1170,6 +1170,15 @@ export class OdooEditor extends EventTarget {
             this._positionToolbar();
         }
     }
+    updateToolbarPosition() {
+        if (
+            this.options.autohideToolbar &&
+            !this.isMobile &&
+            getComputedStyle(this.toolbar).visibility === 'visible'
+        ) {
+            this._positionToolbar();
+        }
+    }
     _positionToolbar() {
         const OFFSET = 10;
         let isBottom = false;
