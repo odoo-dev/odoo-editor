8c40f48569f3d7fdde1e50761b4053c7fdc2e6a7
Antoine Guenet <age@odoo.com>
2021-06-28 12:20:50 +0200

    [IMP] paste text safely in editor without wrapping code in PRE tags
    The system to detect if the text that was pasted contained javascript or
    xml was weird, buggy and risky. This reverts it so we can make use of
    insertText instead of insertHTML and be perfectly safe. We also preserve
    linebreaks in HTML.

--- src/editor.js
+++ src/editor.js
@@ -1544,40 +1544,6 @@ export class OdooEditor extends EventTarget {
         return container.innerHTML;
     }
     /**
-     * Prepare clipboard data (text/plain) for safe pasting into the editor.
-     *
-     * @private
-     * @param {string} clipboardData
-     * @returns {string}
-     */
-    _prepareTextClipboardData(clipboardData) {
-        const isXML = !!clipboardData.match(
-            /<[a-z]+[a-z0-9-]*( [^>]*)*>[\s\S\n\r]*<\/[a-z]+[a-z0-9-]*>/i,
-        );
-        const isJS =
-            !isXML &&
-            !!clipboardData.match(/\(\);|this\.|self\.|function\s?\(|super\.|[a-z0-9]\.[a-z].*;/i);
-
-        const container = document.createElement('fake-container');
-        const pre = document.createElement('pre');
-        pre.innerHTML = clipboardData
-            .trim()
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            // Get that text as an array of text nodes separated by <br> where
-            // needed.
-            .replace(/(\n+)/g, '<br>');
-
-        if (isJS || isXML) {
-            container.appendChild(pre);
-        } else {
-            for (const node of pre.childNodes) {
-                container.appendChild(node);
-            }
-        }
-        return container.innerHTML;
-    }
-    /**
      * Clean a node for safely pasting. Cleaning an element involves unwrapping
      * its contents if it's an illegal (blacklisted or not whitelisted) element,
      * or removing its illegal attributes and classes.
@@ -2170,10 +2136,15 @@ export class OdooEditor extends EventTarget {
                         );
                     }
                 } else if (splitAroundUrl[i] !== '') {
-                    this.execCommand(
-                        'insertHTML',
-                        this._prepareTextClipboardData(splitAroundUrl[i]),
-                    );
+                    const textFragments = splitAroundUrl[i].split('\n');
+                    let textIndex = 1;
+                    for (const textFragment of textFragments) {
+                        this.execCommand('insertText', textFragment);
+                        if (textIndex < textFragments.length) {
+                            this._applyCommand('oShiftEnter');
+                        }
+                        textIndex++
+                    }
                 }
             }
         }
